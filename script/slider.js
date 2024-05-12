9f0762f0ce4ea2d8b2bd35793ee12bfcda4f7c7c
Klim D. <klimdudko@outlook.com>
2024-05-12 21:48:25 +0200

    header + subheader + slider 100%

---
+++ script/slider.js
@@ -0,0 +1,32 @@
+import image1 from '../src/img/slider_img/1.jpg';
+import image2 from '../src/img/slider_img/2.jpg';
+import image3 from '../src/img/slider_img/3.jpeg';
+
+    const images = [image1, image2, image3];
+    
+    let activeImage = 0;
+    const sliderPlace = document.querySelector('.slider__img');
+    
+    const updateSlide = (imageIndex) => {
+        sliderPlace.innerHTML = '';
+        const img = document.createElement('img'); 
+            img.alt = 'slider Image';
+            img.className = 'slider__img-item';
+            img.src = images[imageIndex];
+        sliderPlace.append(img);
+    };
+    updateSlide(activeImage);
+    
+    const nextImageGen = () => {
+        activeImage = (activeImage + 1) % images.length;
+        updateSlide(activeImage);
+    }
+    
+    const prevImageGen = () => {
+        activeImage = (activeImage - 1 + images.length) % images.length;
+        updateSlide(activeImage);
+    }
+    
+    document.querySelector('.slider__controls-left').addEventListener('click', prevImageGen);
+    document.querySelector('.slider__controls-right').addEventListener('click', nextImageGen);
+
